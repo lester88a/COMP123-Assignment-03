2714cf53385db6ca4e8dd1cd526f10faa78e58e0
lester88a <lester88a@gmail.com>
2015-02-16 20:34:25 -0500

    CREATE Interface class

--- Assignment-03/TerrestrialPlanet.cs
+++ Assignment-03/TerrestrialPlanet.cs
@@ -8,5 +8,44 @@ namespace Assignment_03
 {
     class TerrestrialPlanet
     {
+        //Create Private Instance Variables+++++++++++++++++++++++
+        private bool _oxygen;
+
+        //Create Constructor Mehotd+++++++++++++++++++++++++++++++++
+        public TerrestrialPlanet(bool oxygen, string name,
+            double diameter, double mass)
+        {
+            this._oxygen = oxygen;
+        }
+
+        //Create Public Methods++++++++++++++++++++++++++++++++++++
+        public bool HasMoons()
+        {
+            //Implement the HasMoons Method
+            Planet IHasMoons = new Planet("Mars", 100, 10);
+
+            //If MoonCount >0, return true
+            if (IHasMoons.MoonCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Habitable()
+        {
+            //Implement the Habitable Method
+            if (this._oxygen == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
