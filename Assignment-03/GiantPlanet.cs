a019b0081316a34acc5146fba0b982ccfc3cbbcd
lester88a <lester88a@gmail.com>
2015-02-16 19:14:32 -0500

    Create Giant Planet Class

---
+++ Assignment-03/GiantPlanet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_03
+{
+    class GiantPlanet
+    {
+        //Creat Private Instance Variables+++++++++++++++++++++++
+        private string _type;
+    }
+}
--- Assignment-03/Program.cs
+++ Assignment-03/Program.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * https://github.com/lester88a/COMP123-Assignment-03.git
+ * Course: COMP-123
+ * Student Name: LI XU
+ * Student ID: 300735072
+ */
 namespace Assignment_03
 {
     class Program
